c6d32182f978382e41c9fd5ee9e1e3daa1248388
Moe Yang <myang@scramsystems.com>
2022-02-09 21:07:01 -0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
