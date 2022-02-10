f6ad9aa185556cb9bc68244a821440a7ff7a15cc
Moe Yang <yhmgood0130@gmail.com>
2022-02-10 00:50:07 -0500

    added css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { Navbar, Welcome, Footer, Loader, Services, Transactions } from './compo
 
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen">
-      <div className='container'>
-        
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
       </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
